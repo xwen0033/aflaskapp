681512d2e456ca20279e04eb93ac2ed9c5381290
Xin Wen <xin.wen20@imperial.ac.uk>
2023-11-13 22:51:10 +0000

    adding example unit tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,6 @@
 import unittest
 from src.app import app
+from unittest.mock import patch, MagicMock
 
 class FlaskAppTestCase(unittest.TestCase):
 
@@ -10,11 +11,22 @@ class FlaskAppTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_home_endpoint(self):
+    def test_home_endpoint_get(self):
         # Test the / endpoint
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        # self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+        assert b'Data Analyzer' in response.data
 
-if __name__ == '__main__':
-    unittest.main()
+    @patch('src.routes.analyze_folder_word_count')
+    def test_home_endpoint_post(self, mock_analyse):
+        mock_analyse.return_value = "Mocked Data"
+        post_data = {'folder_path': 'test_folder'}
+        with patch(
+            "src.routes.plot_word_count",
+            return_value='test_plot'
+        ) as mock_plot:
+            response = self.app.post('/', data=post_data, follow_redirects=True)
+            mock_plot.assert_called_once_with("Mocked Data")
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'test_plot', response.data)
+
---
+++ tests/test_utils.py
@@ -0,0 +1,57 @@
+import pytest
+from unittest.mock import patch
+from src.utils import read_csv_from_path, save_output
+from contextlib import ExitStack as DoesNotRaise
+
+
+def test_valid_csv_file():
+    with patch(
+            "src.utils.os.path.isfile",
+            return_value=True
+    ) as os_isfile_mock, patch(
+        "src.utils.pd.read_csv",
+        return_value='mock result'
+    ) as pd_read_mock:
+        test_file_path = 'notaCSVfile'
+        with pytest.raises(Exception) as exc_info:
+            observed = read_csv_from_path(test_file_path)
+            os_isfile_mock.assert_called_once()
+            pd_read_mock.assert_not_called()
+            assert observed is None
+            assert exc_info is ValueError("Invalid file format. Please provide a CSV file.")
+
+        test_file_path = 'valid.csv'
+        with pytest.raises(Exception) as exc_info:
+            observed = read_csv_from_path(test_file_path)
+            os_isfile_mock.assert_called_once()
+            pd_read_mock.assert_called_once_with(test_file_path)
+            assert observed == 'mock result'
+            assert type(exc_info.value.__cause__) is DoesNotRaise()
+
+    with patch(
+            "src.utils.os.path.isfile",
+            return_value=False
+    ) as not_file_mock, patch(
+        "src.utils.pd.read_csv"
+    ) as read_not_called:
+        test_file_path = 'not a file'
+        with pytest.raises(Exception) as exc_info:
+            observed = read_csv_from_path(test_file_path)
+            not_file_mock.assert_called_once()
+            read_not_called.assert_not_called()
+            assert observed is None
+            assert exc_info is ValueError("Please enter a valid file path and make sure the file exists")
+
+
+
+def test_save_output():
+    with patch(
+        "src.utils.open"
+    ) as mock_open, patch(
+        "src.utils.json.dump"
+    )as mock_json_dump:
+        output = {'key': 'value'}
+        path = 'path/to/mock_output.json'
+        save_output(output, path)
+        mock_open.assert_called_once_with(path, 'w')
+        mock_json_dump.assert_called_once_with(output, mock_open().__enter__())
